62682e76564ed2e1e434a51064dc6345d22b2c50
SASAKI SENA <sasaki07sena@gmail.com>
2026-07-10 23:03:54 +0900

    add: phase1 - print, variables, datatypes

---
+++ phase1_basics/02_variables.py
@@ -0,0 +1,10 @@
+# 自己紹介
+name = "Sena"
+age = 23
+hometown = "Kanagawa"
+
+print(name,age,hometown)
+
+name = "Taro"
+
+print(name)
---
+++ phase1_basics/03_datatypes.py
@@ -0,0 +1,14 @@
+name = "Sena"
+age = 23
+tall = 168.5
+todaydone = "今日勉強した"
+
+print(type(name))
+print(type(age))
+print(type(tall))
+print(type(todaydone))
+print(type(1))
+print(type(0))
+
+is_student = True
+print(type(is_student))
